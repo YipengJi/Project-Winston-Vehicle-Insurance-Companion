69b93b82fd1e9e2d5ebfe3a198665333ea006b28
Andrew Mourcos <amourcos@gmail.com>
2019-08-27 16:28:19 -0400

    post-hack maintenance

---
+++ WIP/winston10 screwed/app/src/main/java/io/github/andrewmourcos/winston10/MainActivity.java
@@ -0,0 +1,27 @@
+package io.github.andrewmourcos.winston10;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button start_btn = (Button)findViewById(R.id.start_btn);
+
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, StartActivity.class));
+            }
+        });
+    }
+}
